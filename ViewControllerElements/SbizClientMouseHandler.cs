c9e8eed6431205985dc6d8b2c9797a07dd0e0edf
gugu91 <gporce@gmail.com>
2014-11-05 16:42:58 +0100

    mouse fully working

--- ViewControllerElements/SbizClientMouseHandler.cs
+++ ViewControllerElements/SbizClientMouseHandler.cs
@@ -12,25 +12,23 @@ namespace Sbiz.Client
     {
         public static void MouseMove(SbizClientRunningUC uc, Control sender, MouseEventArgs e)
         {
-            System.Drawing.Point p = uc.PointToClient(sender.PointToScreen(e.Location));
-
-            SbizMouseEventArgs smea = new SbizMouseEventArgs(e.Button, e.Clicks, e.Delta, p.X, p.Y, uc.Bounds.Width, uc.Bounds.Height);
-            SbizMessage m = new SbizMessage(SbizMessageConst.MOUSE_MOVE, smea.ToByteArray());
-            SbizClientController.ModelSetData(m.ToByteArray());
+            BasicMouse(uc, sender, e);
         }
 
         public static void MouseUp(SbizClientRunningUC uc, Control sender, MouseEventArgs e)
         {
-            System.Drawing.Point p = uc.PointToClient(sender.PointToScreen(e.Location));
-            SbizMouseEventArgs smea = new SbizMouseEventArgs(e.Button, e.Clicks, e.Delta, p.X, p.Y, uc.Bounds.Width, uc.Bounds.Height);
-            SbizMessage m = new SbizMessage(SbizMessageConst.MOUSE_MOVE, smea.ToByteArray());
-            SbizClientController.ModelSetData(m.ToByteArray());
+            BasicMouse(uc, sender, e);
         }
 
         public static void MouseWheel(SbizClientRunningUC uc, Control sender, MouseEventArgs e)
         {
+            BasicMouse(uc, sender, e);
+        }
+
+        private static void BasicMouse(SbizClientRunningUC uc, Control sender, MouseEventArgs e)
+        {
             System.Drawing.Point p = uc.PointToClient(sender.PointToScreen(e.Location));
-            SbizMouseEventArgs smea = new SbizMouseEventArgs(e.Button, e.Clicks, e.Delta, p.X, p.Y, uc.Bounds.Width, uc.Bounds.Height);
+            SbizMouseEventArgs smea = new SbizMouseEventArgs(e.Button, e.Clicks, e.Delta/SystemInformation.MouseWheelScrollDelta, p.X, p.Y, uc.Bounds.Width, uc.Bounds.Height);
             SbizMessage m = new SbizMessage(SbizMessageConst.MOUSE_MOVE, smea.ToByteArray());
             SbizClientController.ModelSetData(m.ToByteArray());
         }
