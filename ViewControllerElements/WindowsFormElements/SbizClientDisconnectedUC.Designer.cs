2774e20a649110151505d266789650dfef01d271
gugu91 <gporce@gmail.com>
2014-11-23 12:12:59 +0100

    added sbiz client disconnected

---
+++ ViewControllerElements/WindowsFormElements/SbizClientDisconnectedUC.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Sbiz.Client.ViewControllerElements.WindowsFormElements
+{
+    partial class SbizClientDisconnectedUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.EnterButton);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.MaximumSize = new System.Drawing.Size(308, 78);
+            this.panel1.MinimumSize = new System.Drawing.Size(308, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(308, 78);
+            this.panel1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Location = new System.Drawing.Point(-4, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 49);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Client Disconnected";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.EnterButton.Location = new System.Drawing.Point(107, 52);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(75, 23);
+            this.EnterButton.TabIndex = 5;
+            this.EnterButton.Text = "Ok";
+            this.EnterButton.UseVisualStyleBackColor = false;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // SbizClientDisconnectedUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "SbizClientDisconnectedUC";
+            this.Size = new System.Drawing.Size(314, 84);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button EnterButton;
+    }
+}
---
+++ ViewControllerElements/WindowsFormElements/SbizClientDisconnectedUC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sbiz.Client.ViewControllerElements.WindowsFormElements
+{
+    public partial class SbizClientDisconnectedUC : UserControl
+    {
+        public SbizClientDisconnectedUC()
+        {
+            InitializeComponent();
+        }
+
+        public void Alert(string message)
+        {
+            label1.Text = message;
+            this.Enabled = true;
+            this.Visible = true;
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            this.Visible = false;
+        }
+    }
+}
